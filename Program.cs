3bd426583ae0386327076e482237a143c6ff9349
Marcos Chavarría Jiménez <mchavarria@cajadeande.fi.cr>
2021-12-15 14:49:36 -0600

    inicial

--- Program.cs
+++ Program.cs
@@ -5,9 +5,6 @@ using Azure.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
-builder.Configuration.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
-
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
